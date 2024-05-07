d155a9d23c7277c4e281cfdf8446d3863b0f4f9d
kotov <kotov@M1ROSHA1>
2024-05-07 19:09:37 +0700

    Добавьте файлы проекта.

---
+++ YESorNO/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace YESorNO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGenerate.Location = new System.Drawing.Point(271, 131);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(132, 45);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Да или Нет";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(204, 43);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(272, 82);
+            this.resultListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(704, 263);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.btnGenerate);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "Yes Or No";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListBox resultListBox;
+    }
+}
+
---
+++ YESorNO/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YESorNO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            double p = 0.5;  // вероятность
+            Random rand = new Random();
+            bool result = rand.NextDouble() < p;  // Генерация результата с вероятностью p
+            
+            resultListBox.Items.Clear();
+            resultListBox.Items.Add(result ? "Да" : "Нет");
+        }
+    }
+}
